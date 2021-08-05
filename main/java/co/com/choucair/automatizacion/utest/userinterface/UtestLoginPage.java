9c4b63edbf97bb39a77a07558d71aea0e7458d8f
ECanoLo <87869203+ECanoLo@users.noreply.github.com>
2021-08-04 23:14:14 -0500

    Update UtestLoginPage.java

--- main/java/co/com/choucair/automatizacion/utest/userinterface/UtestLoginPage.java
+++ main/java/co/com/choucair/automatizacion/utest/userinterface/UtestLoginPage.java
@@ -5,12 +5,12 @@ import net.serenitybdd.screenplay.targets.TargetBuilder;
 import org.openqa.selenium.By;
 
 public class UtestLoginPage extends PageObject {
-    public static final Target LOGIN_BUTTON = Target.the("Button that shows us the form to login").located
-            (By.xpath("//div[@class='d-none d.lg.block']//strong[contains(text(),'Log In')]"));
     public static final Target INPUT_USER = Target.the("Where do we write the emailuser").located
-            (By.id("emailUser"));
-    public static final Target INPUT_PASSWORD = Target.the("Where do we write the password").located(By.id("password"));
+            (By.id("//*[@id="username"]"));
+    public static final Target INPUT_PASSWORD = Target.the("Where do we write the password").located(By.id("//*[@id="password"]"));
     public static final Target ENTER_BUTTON = Target.the("button to confirm login").located
-            (By.xpath("//button[contains(@class, 'btn btn-primary')]"));
+            (By.xpath("//*[@id="kc-login"]"));
+    public static final Target LOGIN_BUTTON = Target.the("Button that open Academy").located
+            (By.xpath("/html/body/ui-view/nav-sidebar/div/nav/div[3]/nav-sidebar-item[2]/div/a/div[2]"));
 
 }
